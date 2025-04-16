3a634f54df4835b270d5bf9be166c867d221034b
Gabriel Pimentel <gabrielsopimentel@gmail.com>
2025-04-15 22:33:04 -0300

    Initial commit

---
+++ src/main/java/br/com/dio/avainvest/AvainvestApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.avainvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvainvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvainvestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dio/avainvest/AvainvestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.avainvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvainvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
